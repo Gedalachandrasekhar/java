bf79c423a562a6a6b29c82c92cfd8ece8f071c73
cherry <chandrasekhar.gedala@gmail.com>
2025-05-06 15:09:43 +0530

    correct int

--- Variable/int.java
+++ Variable/int.java
@@ -1,9 +1,11 @@
-class int {
+package Variable;
+
+class int1 {
     public static void main(String a[])
     {
         int x = 10;
         int y = 20;
         int result= x+y;
-        System.out.print
+        System.out.print(result);
     }    
 }
